dc84bd62c64f3d864fb58f425df03f2af966c6dc
Megan Truong <mtruong3@uw.edu>
2019-03-06 19:57:45 -0800

    add exercise 2

--- exercise2.py
+++ exercise2.py
@@ -7,3 +7,51 @@ import dash_html_components as html
 # Exercise 2
 # Create another visualization of your choice of data and chart type.
 # You can use pandas to help loading data, or just hard-coded the data is fine.
+
+# initialize Dash app and initialize the static folder
+app = dash.Dash(__name__, static_folder='static')
+df = pd.read_csv('cereal.csv')
+# set layout of the page
+app.layout = html.Div(children=[
+
+    # set the page heading
+    html.H1(children='Cereal'),
+
+    # set the description underneath the heading
+    html.Div(children='''
+        Different cereal brand calories compared to corresponding sugars
+    '''),
+
+    # append the visualization to the page
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            # configure the data
+            'data': [
+                # This is how we define a scatter plot. Note that it also uses "go.Scatter",
+                # but with the mode to be only "markers"
+                go.Scatter(
+                    x=df['calories'],
+                    y=df['sugars'],
+                    mode='markers',
+                    text=df['name'],  # This line sets the vehicle name as the points' labels.
+                    marker={
+                        'size': 10,
+                        'opacity': 0.8  # By making the points a bit transparent, it can alleviate the occlusion issue
+                    }
+                )
+            ],
+            'layout': {
+                'title': 'Cereal Dataset',
+                # It is always a good practice to have axis labels.
+                # This is especially important in this case as the numbers are not trivial
+                'xaxis': {'title': 'Calories'},
+                'yaxis': {'title': 'Sugars (grams)'},
+            }
+        }
+    )
+
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
