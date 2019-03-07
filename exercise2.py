61c238ca051e1732e1b1f0555fde0257aa8fa377
Megan Truong <mtruong3@uw.edu>
2019-03-06 20:18:54 -0800

    add imports

--- exercise2.py
+++ exercise2.py
@@ -2,8 +2,8 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-
-
+import plotly.graph_objs as go
+import pandas as pd
 # Exercise 2
 # Create another visualization of your choice of data and chart type.
 # You can use pandas to help loading data, or just hard-coded the data is fine.
